e6392a522e8016e5c61a197c28334d0d067ac248
Alexandru Cristian <alex.cristian@me.com>
2014-05-22 20:01:30 +0300

    Tweaked permitted characters

--- src/com/nexusplay/security/QuerySanitizer.java
+++ src/com/nexusplay/security/QuerySanitizer.java
@@ -5,7 +5,7 @@ public class QuerySanitizer {
 	private final static String allowedCharacters="abcdefghijklmnopqrstuvwxyz"
 			+ "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 			+ "1234567890"
-			+ "!?:@,._-";
+			+ "!?:@,._- ";
 	public static String sanitizeQueryString(String s) throws Exception{
 		s=s.trim();
 		for(int i=0; i< s.length(); i++){
