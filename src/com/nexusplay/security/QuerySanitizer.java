6964ddcea64d387c2db2d7518b9a58a249815f1a
TheShadows <alex.cristian@me.com>
2014-07-30 11:34:57 +0300

    Quick fix for Cyrillic & other unicode characters

--- src/com/nexusplay/security/QuerySanitizer.java
+++ src/com/nexusplay/security/QuerySanitizer.java
@@ -2,14 +2,13 @@ package com.nexusplay.security;
 
 public class QuerySanitizer {
 
-	private final static String allowedCharacters="abcdefghijklmnopqrstuvwxyzăâîțș"
-			+ "ABCDEFGHIJKLMNOPQRSTUVWXYZĂÂÎȚȘ"
+	private final static String allowedCharacters= "ăâîțșĂÂÎȚȘ"
 			+ "1234567890"
-			+ "!?:@,._- «»" + '\n' + "\"";
+			+ "¡!?:@,._- «»" + '\n' + "\"";
 	public static String sanitizeQueryString(String s) throws Exception{
 		s=s.trim();
 		for(int i=0; i< s.length(); i++){
-			if(allowedCharacters.indexOf(s.charAt(i))==-1){
+			if(!Character.isDigit(s.charAt(i)) && !Character.isLetter(s.charAt(i)) && allowedCharacters.indexOf(s.charAt(i))==-1){
 				throw new Exception("Invalid characters present! Please restate your query.");
 			}
 		}
